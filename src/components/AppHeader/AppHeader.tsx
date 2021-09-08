64114dc0d4a38856c66f80a6c3743f84afa6685d
Максим Ермохин <mvyermok@mts.ru>
2021-09-08 10:26:57 +0500

    in progress

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import settingsIcon from 'assets/icons/settings.svg';
 import styles from './AppHeader.module.css';
+import AppButton from 'components/AppButton/AppButton';
 
 export default function AppHeader() {
-  return <div className={styles.wrapper}>Guitar Note Trainer</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.logo}>Play This Note!</div>
+      <div className={styles.settings}>
+        <AppButton onClick={() => {}}
+                   secondary>
+          <img src={settingsIcon}
+               alt="Settings" />
+        </AppButton>
+      </div>
+    </div>
+  );
 }
