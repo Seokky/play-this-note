286fd544a8b6205de4eb2195e9e7d95aae232874
Максим Ермохин <mvyermok@mts.ru>
2021-09-20 12:49:32 +0500

    back button in header

--- src/components/App/AppHeader.tsx
+++ src/components/App/AppHeader.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 import settingsIcon from 'assets/icons/settings.svg';
+import angleDown from 'assets/icons/angle-down.svg';
 import styles from 'assets/styles/components/app/AppHeader.module.css';
 import AppButton from 'components/app/AppButton';
 
 export default function AppHeader() {
+  const location = useLocation();
+  const history = useHistory();
+
+  const goBackInHistory = () => {
+    history.goBack();
+  };
+
   return (
     <div className={styles.wrapper}>
-      <Link to="/">
-        <div className={styles.logo}>Play This Note!</div>
-      </Link>
+      {
+        (location.pathname === '/')
+          ?
+            <Link to="/">
+              <div className={styles.logo}>Play This Note!</div>
+            </Link>
+          :
+            <div className={styles.back}>
+              <AppButton onClick={goBackInHistory} secondary>
+                <img src={angleDown} alt="Back" />
+              </AppButton>
+            </div>
+      }
 
       <div className={styles.settings}>
         <AppButton onClick={() => {}}
